96cb163c1bf777ee7705e446cdefdf7a318e6188
Amar Kisoensingh <amar@kisoensingh.sh>
2024-12-18 00:48:32 +0000

    Fix events width on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Header, Events } from "@/components";
 
 export default function Home() {
   return (
-    <div className="grid w-full max-w-96 grid-rows-[auto,1fr]">
+    <div className="grid w-full grid-rows-[auto,1fr] md:max-w-96">
       <Header />
       <Events />
     </div>
